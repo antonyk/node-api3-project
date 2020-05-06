2a7505ed26804797a4f6951cd1fa5b4330987b16
Anthony K <antonyk@users.noreply.github.com>
2020-05-06 17:53:13 -0400

    intial config/install

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+// config server
 const server = express();
+// global middleware
+// 1. enable json
+server.use(express.json());
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
